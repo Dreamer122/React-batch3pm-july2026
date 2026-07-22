67fc866cae1fcf92b773907134fc498ac09043df
Dreamer122 <infotechdreamer@gmail.com>
2026-07-22 16:15:41 +0530

    forms

---
+++ FourthProject/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Registration from './Registration'
+
+const App = () => {
+  return (
+    <>
+    
+    <div className="text-4xl">App</div>
+    <Registration/>
+    </>
+  )
+}
+
+export default App
